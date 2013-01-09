2d422c0b8637db623f27fcbc83b6657d166a7b90
unknown <clange@M1006807.cg.na.jci.com>
2013-01-09 17:32:32 -0600

    Added Hello Project Files

---
+++ Hello/Hello.cpp
@@ -0,0 +1,16 @@
+// Hello.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include <iostream>
+
+using namespace std;
+
+int _tmain(int argc, _TCHAR* argv[])
+{
+
+	cout << "Hello" << endl;
+
+	return 0;
+}
+
